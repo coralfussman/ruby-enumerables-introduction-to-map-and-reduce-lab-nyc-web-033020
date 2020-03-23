45e9c6a45ed2b5c372818618ccd563037bfe7ccc
Coral Fussman <coralfussman@gmail.com>
2020-03-23 11:56:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# My Code here....
+def map_to_negativize(source_array)
+
+my_negative_array =[]
+index =0
+while index < source_array.length
+array_results = source_array[index] * -1
+my_negative_array << array_results
+index += 1
+
+end
+
+end
