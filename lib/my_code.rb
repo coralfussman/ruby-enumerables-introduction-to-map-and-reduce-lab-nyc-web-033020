d9db1af7e606cbdb31f864d33588f9433e5bc4d6
Coral Fussman <coralfussman@gmail.com>
2020-03-23 13:00:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -75,16 +75,14 @@ end
 
 
 def reduce_to_any_true(source_array)
-  
+
   index = 0
   while index < source_array.length do
     if source_array[index]
       return true
     end
     index +=1
-    
+
   end
   return false
 end
-
-
