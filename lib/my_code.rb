2e11f338ec96e5643cf102f4b5dc2093ca99c022
Coral Fussman <coralfussman@gmail.com>
2020-03-23 12:33:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,9 +50,9 @@ end
 
 def reduce_to_total(source_array, starting_point)
 
-  index =0
+
   while index < source_array.length
-    source_array[index].sum
-    index += 1
+    source_array[starting_point].sum
+    starting_point += 1
   end
 end
