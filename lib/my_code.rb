1bc1a9c4f8e96412b91501c0ad740e796c189452
Coral Fussman <coralfussman@gmail.com>
2020-03-23 12:15:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,10 @@ end
 def map_to_no_change(source_array)
 
 my_original_array = []
-
-  my_original_array == source_array
-
+index =0
+while index < source_array.length
+  my_original_array << source_array
+else
+nil
+end
 end
