560e86cfaa006a189ecf2eda2b75a7cd31e77d68
Coral Fussman <coralfussman@gmail.com>
2020-03-23 12:35:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-   
+
   while index < source_array.length
     source_array[starting_point]
   reduced_total = source_array[starting_point] += 1
